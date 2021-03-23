f249ee13d66079dbd1cd18a38c86b2a0e3d0e482
qoo332001 <jsps060415@gmail.com>
2021-03-23 17:26:32 +0800

    first commit

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>何敏煌的練習網頁</title>
+	<title>孫的練習網頁</title>
 	<style>
 		body {
-			  background-color: lightblue;
-			}
-
-			h1 {
-			  color: white;
-			  text-align: center;
-			}
-
-			p {
-			  font-family: verdana;
-			  font-size: 20px;
-		}
+			 font-family: 微軟正黑體;
 	</style>
 </head>
 <body>
 
 <h1>歡迎光臨我的網站</h1>
+<button><a href='/newexe1/'>HOME</a></button>
+<button><a href='https://cla.nkust.edu.tw'>博雅中心</a></button>
+<button><a href='http://tzukuan.nkust.org'>計畫網站</a></button>
+<select onchange='location.href="/newexe1/"+this.value'>
+	<option value="0323-1.php">請選擇要執行的練習</option>
+	<option value="0323-1.php">0323-1</option>
+	<option value="0323-2.php">0323-2</option>
+</select>
 <hr>
-<p>有什麼問題歡迎連絡 minhuang@nkust.edu.tw</p>
+
+<p>有什麼問題歡迎連絡 0651101@nkust.edu.tw</p>
 
 </body>
 </html>
