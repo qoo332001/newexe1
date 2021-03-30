192f74ddaff9706ca5c12f89be1578cea8b27f48
qoo332001 <jsps060415@gmail.com>
2021-03-30 17:12:44 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<h1>歡迎光臨我的網站</h1>
+<button><a href='/newexe1/'>HOME</a></button>
+<button><a href='https://cla.nkust.edu.tw'>博雅中心</a></button>
+<button><a href='http://tzukuan.nkust.org'>計畫網站</a></button>
+<select onchange='location.href="/newexe1/"+this.value'>
+	<option value="0323-1.php">請選擇要執行的練習</option>
+	<option value="0323-1.php">0323-1</option>
+	<option value="0323-2.php">0323-2</option>
+</select>
+<hr>
--- index.php
+++ index.php
@@ -8,19 +8,7 @@
 	</style>
 </head>
 <body>
-
-<h1>歡迎光臨我的網站</h1>
-<button><a href='/newexe1/'>HOME</a></button>
-<button><a href='https://cla.nkust.edu.tw'>博雅中心</a></button>
-<button><a href='http://tzukuan.nkust.org'>計畫網站</a></button>
-<select onchange='location.href="/newexe1/"+this.value'>
-	<option value="0323-1.php">請選擇要執行的練習</option>
-	<option value="0323-1.php">0323-1</option>
-	<option value="0323-2.php">0323-2</option>
-</select>
-<hr>
-
-<p>有什麼問題歡迎連絡 0651101@nkust.edu.tw</p>
-
+<?php include("header.php"); ?>
+<?php include("footer.php"); ?>
 </body>
 </html>
